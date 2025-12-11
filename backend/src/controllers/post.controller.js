16af59cbc27d131f13fe4e6c67cda46b32bda0bd
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2025-12-11 15:08:00 -0500

    Adding the delete method to the API

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -46,8 +46,22 @@ const updatePost = async (req, res) => {
     }
 };
 
+const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        };
+
+        return res.status(200).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+};
+
 export {
     createPost,
     getPosts,
-    updatePost
+    updatePost,
+    deletePost
 }
